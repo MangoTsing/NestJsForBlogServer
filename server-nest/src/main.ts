28a69a0ef229ef8f8a93c74b9d57b85425c3ae54
MangoTsing <757271842@qq.com>
2019-12-24 20:42:41 +0800

    更改接口统一格式

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(80);
 }
 bootstrap();
