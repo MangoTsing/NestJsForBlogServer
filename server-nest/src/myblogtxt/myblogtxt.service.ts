9684e975e9ec622931523bd75db8bb16c39ad5e6
MangoTsing <757271842@qq.com>
2019-12-30 16:03:42 +0800

    🏠🤮4⃣️👃

--- server-nest/src/myblogtxt/myblogtxt.service.ts
+++ server-nest/src/myblogtxt/myblogtxt.service.ts
@@ -20,39 +20,41 @@ export class MyblogtxtService {
   }
   //获取豆瓣电影列表
   async movieList(): Promise<any> {
-    return await http.get('http://nodejs.cn/index.json', (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
-    
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('请求失败\n' +
-                          `状态码: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('无效的 content-type.\n' +
-                          `期望的是 application/json 但接收到的是 ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // 消费响应数据来释放内存。
-        res.resume();
-        return;
-      }
-    
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          return parsedData;
-        } catch (e) {
-          console.error(e.message);
+    const api = 'http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10';
+    return new Promise(function(resolve, reject) {
+      http.get(api, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('请求失败\n' +
+                            `状态码: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('无效的 content-type.\n' +
+                            `期望的是 application/json 但接收到的是 ${contentType}`);
         }
+        if (error) {
+          reject(error.message);
+          // 消费响应数据来释放内存。
+          res.resume();
+          return;
+        }
+      
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            resolve(parsedData);
+          } catch (e) {
+            reject(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        reject(`出现错误: ${e.message}`);
       });
-    }).on('error', (e) => {
-      console.error(`出现错误: ${e.message}`);
-    });
+    })
   }
   //给单篇文章点star+1
   async postStar (str: postStar): Promise<MyBlogTxt> {
